ad9982f47f2e0e296929a75cf57749ef6c23a11e
Elidion-netizen <sapegaserg@gmail.com>
2024-11-09 20:03:54 +0300

    feat: split method in strings

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -297,8 +297,8 @@ function containsSubstring(str, substring) {
  *   countVowels('aEiOu') => 5
  *   countVowels('XYZ') => 1
  */
-function countVowels(/* str */) {
-  throw new Error('Not implemented');
+function countVowels(str) {
+  return [...str.matchAll(/[aeiouyAEIOUY]/g)].length;
 }
 
 /**
@@ -314,8 +314,9 @@ function countVowels(/* str */) {
  *   isPalindrome('apple') => false
  *   isPalindrome('No lemon, no melon') => true
  */
-function isPalindrome(/* str */) {
-  throw new Error('Not implemented');
+function isPalindrome(str) {
+  const cleanedStr = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+  return cleanedStr === cleanedStr.split('').reverse().join('');
 }
 
 /**
@@ -330,8 +331,14 @@ function isPalindrome(/* str */) {
  *   findLongestWord('A long and winding road') => 'winding'
  *   findLongestWord('No words here') => 'words'
  */
-function findLongestWord(/* sentence */) {
-  throw new Error('Not implemented');
+function findLongestWord(sentence) {
+  return sentence
+    .split(' ')
+    .reduce(
+      (longest, currentWord) =>
+        currentWord.length > longest.length ? currentWord : longest,
+      ''
+    );
 }
 
 /**
@@ -344,8 +351,8 @@ function findLongestWord(/* sentence */) {
  *   reverseWords('Hello World') => 'olleH dlroW'
  *   reverseWords('The Quick Brown Fox') => 'ehT kciuQ nworB xoF'
  */
-function reverseWords(/* str */) {
-  throw new Error('Not implemented');
+function reverseWords(str) {
+  return str.split(' ').reverse().join(' ').split('').reverse().join('');
 }
 
 /**
